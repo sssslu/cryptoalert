67e9b2c11ee781fd451baa8a14dcca7f9d037c68
sssslu <yhsyl1127@naver.com>
2025-05-26 17:39:32 +0900

    threshold modified

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import time
 import requests
 import winsound
+import os
 
 # === 설정 ===
 SYMBOL = "BTCUSDT"
 GRANULARITY = "1m"
-CHECK_INTERVAL = 10              # 10초마다 체크
-PRICE_CHANGE_THRESHOLD = 0.01    # 1% 이상 변화 감지
+CHECK_INTERVAL = 5              # 5초마다 체크
+PRICE_CHANGE_THRESHOLD = 0.004    # 변화 감지 threshold
 PRODUCT_TYPE = "usdt-futures"
 
 # === 비트겟 현재 가격 받아오기 ===
@@ -40,17 +41,21 @@ def beeploww():
 
 # === 메인 루프 ===
 def main():
-    print("🚨Bitcoin 급등락 감지기 by Slu - using BITGET API")
+    print("🚨Bitcoin 급등락 감지기🚨 by Slu - Bitget API")
+    beeploww()
+    beeplow()
+    beep()
     beepshort()
     prev_price = get_latest_price()
     if prev_price is None:
         print("초기 가격 로딩 실패")
         return
 
-    print("")#사용자 이름이나 상표
+    print("")
+    line_count = 0
 
     while True:
-        time.sleep(CHECK_INTERVAL)
+        
         try:
             curr_price = get_latest_price()
             if curr_price is None:
@@ -59,23 +64,22 @@ def main():
 
             delta = (curr_price - prev_price) / prev_price
             print(f" {curr_price:.2f} |  {delta*100:.2f}%")
+            line_count += 1
 
             if abs(delta) >= PRICE_CHANGE_THRESHOLD:
-                print("@@@@ 10초 내 급변 감지함 @@@@")
-                beeplow()
-                beep()
-                beeplow()
-                beep()
-                beeplow()
-                beep()
-                beeplow()
-                beep()
-                beeplow()
-                beep()
-                beeplow()
-                beep()
+                print("@@@@ 급변 감지함 @@@@")
+                for _ in range(7):
+                    beeplow()
+                    beep()
 
-            prev_price = curr_price  # 현재 가격을 다음 비교용으로 저장
+            # === 40줄마다 화면 클리어 ===
+            if line_count >= 40:
+                os.system('cls')  # Windows에서 콘솔 clear
+                print("🚨Bitcoin 급등락 감지기🚨 by Slu - Bitget API")
+                line_count = 0
+
+            prev_price = curr_price
+            time.sleep(CHECK_INTERVAL)
 
         except Exception as e:
             print(f"에러: {e}")
